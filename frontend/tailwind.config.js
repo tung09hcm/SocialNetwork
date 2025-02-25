c42c0dd114037307c35e5be94459be629aef8b98
tung <tung.bui2004lk@hcmut.edu.vn>
2025-02-25 22:12:41 +0700

    safe class

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,14 +2,15 @@ import daisyui from "daisyui";
 import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: [ "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
 	},
 	plugins: [daisyui],
-
+	
 	daisyui: {
 		themes: [
+			"light",
 			{
 				black: {
 					...daisyUIThemes["black"],
@@ -19,4 +20,8 @@ export default {
 			},
 		],
 	},
+	safelist: [
+		"avatar"
+	]
+
 };
