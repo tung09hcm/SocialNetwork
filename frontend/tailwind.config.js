35b6acd658f51f7ad8b641b59a7bed5614d18673
tung <tung.bui2004lk@hcmut.edu.vn>
2025-02-25 22:55:37 +0700

    move daisyui to dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,12 +2,12 @@ import daisyui from "daisyui";
 import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [ "./src/**/*.{js,ts,jsx,tsx}"],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
 	},
 	plugins: [daisyui],
-	
+
 	daisyui: {
 		themes: [
 			"light",
@@ -20,8 +20,4 @@ export default {
 			},
 		],
 	},
-	safelist: [
-		"avatar"
-	]
-
 };
