79bf280e43b360784b3958e0511498f811d4b2f5
tung <tung.bui2004lk@hcmut.edu.vn>
2025-05-19 22:30:06 +0700

    small changes in login and register ui

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -60,11 +60,11 @@ const SignUpPage = () => {
 	};
 
 	return (
-		<div className='max-w-screen-xl mx-auto flex h-screen px-10'>
+		<div className='max-w-screen-xl mx-auto flex h-screen px-10 border-b border-gray-700'>
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
 					<XSvg className='w-24 lg:hidden fill-white' />
-					<h1 className='text-4xl font-extrabold text-white'>Join today.</h1>
+					<h1 className='text-4xl font-extrabold text-black'>Join today.</h1>
 					<label className='input input-bordered rounded flex items-center gap-2'>
 						<MdOutlineMail />
 						<input
@@ -117,7 +117,7 @@ const SignUpPage = () => {
 					{isError && <p className='text-red-500'>{error.message}</p>}
 				</form>
 				<div className='flex flex-col lg:w-2/3 gap-2 mt-4'>
-					<p className='text-white text-lg'>Already have an account?</p>
+					<p className='text-black text-lg'>Already have an account?</p>
 					<Link to='/login'>
 						<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Sign in</button>
 					</Link>
